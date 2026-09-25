09e5cfb445a9bc009e46eaeae010010c71dd2b56
sunasouvenir <suna.souvenir@gmail.com>
2026-09-25 21:02:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'suna-planner-v15';
+const CACHE_NAME = 'suna-planner-v33';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (event) {
